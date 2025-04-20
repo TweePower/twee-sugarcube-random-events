cd80ce50cc923a23289c14a75e1fabbeb5620e42
TweePower <tweepower@gmail.com>
2025-04-20 21:10:08 +0300

    chore: remove unused file

--- src/type/Variables.d.ts
+++
@@ -1,7 +0,0 @@
-export type LimitationStrategyVariableType = {
-    randomEventHistory?: string | undefined,
-    randomEventFiredEvents?: string | undefined,
-    randomEventFiredTags?: string | undefined,
-    randomEventEnabledEvents?: string | undefined,
-    randomEventDisabledEvents?: string | undefined,
-};
